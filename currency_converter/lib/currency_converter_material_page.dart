4b8b3974f6ef0c49d87bcf60e7253c232323e4e8
paranietharan <paranie20@gmail.com>
2024-11-22 23:09:58 +0530

    border

--- currency_converter/lib/currency_converter_material_page.dart
+++ currency_converter/lib/currency_converter_material_page.dart
@@ -5,13 +5,24 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        backgroundColor: Colors.white,
+    final border = OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Colors.black,
+                    width: 3.0,
+                    style: BorderStyle.solid,
+                    strokeAlign: BorderSide.strokeAlignOutside,
+                  ),
+                  borderRadius: BorderRadius.circular(60.0),
+                  gapPadding: 10.0,
+                );
+
+    return Scaffold(
+        backgroundColor: Colors.blueGrey,
         body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               '0',
               style: TextStyle(
                 fontSize: 48,
@@ -21,7 +32,41 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
             ),
             TextField(
               decoration: InputDecoration(
-                labelText: 'Enter amount',
+                // labelText: 'Enter amount',
+                // labelStyle: TextStyle(
+                //   color: Colors.black,
+                // ),
+                // helperText: 'Enter amount',
+                hintText: "Please enter the amount in USD",
+                hintStyle: const TextStyle(
+                  color: Colors.black,
+                ),
+                prefixIcon: const Icon(Icons.monetization_on),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                // focusedBorder: OutlineInputBorder(
+                //   borderSide: const BorderSide(
+                //     color: Colors.black,
+                //     width: 3.0,
+                //     style: BorderStyle.solid,
+                //     strokeAlign: BorderSide.strokeAlignOutside,
+                //   ),
+                //   borderRadius: BorderRadius.circular(60.0),
+                //   gapPadding: 10.0,
+                // ),
+                // enabledBorder: OutlineInputBorder(
+                //   borderSide: const BorderSide(
+                //     color: Colors.black,
+                //     width: 3.0,
+                //     style: BorderStyle.solid,
+                //     strokeAlign: BorderSide.strokeAlignOutside,
+                //   ),
+                //   borderRadius: BorderRadius.circular(60.0),
+                //   gapPadding: 10.0,
+                // ),
+                focusedBorder: border,
+                enabledBorder: border,
               ),
               keyboardType: TextInputType.number,
             ),
